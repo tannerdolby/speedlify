a9ecb9e6b429453c4d1046a1009025340d485ecd
Tanner <dolb.tanner@gmail.com>
2020-10-10 11:22:32 -0700

    Adding my personal site to API urls data

---
+++ _data/sites/tannersWebsite.js
@@ -0,0 +1,16 @@
+module.exports = {
+    name: "My personal Website",
+    description: "Tanner Dolby's personal website.",
+    options: {
+        frequency: 60 * 13,
+    },
+    urls: [
+        "https://tannerdolby.netlify.app/",
+        "https://tannerdolby.netlify.app/about/",
+        "https://tannerdolby.netlify.app/writing/",
+        "https://tannerdolby.netlify.app/art/",
+        "https://tannerdolby.netlify.app/writing/web/11ty-iterate-global-data/",
+        "https://tannerdolby.netlify.app/writing/web/js-client-side/",
+        "https://tannerdolby.netlify.app/writing/web/overthewire-games/"
+    ]
+};
